131ee67eac09ce3c6b6cd8999f288fce96eb0036
Less <duduless2020@gmail.com>
2025-05-27 16:00:18 -0300

    Delete SecurityHub/src/main directory

--- SecurityHub/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.securityhub {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.securityhub to javafx.fxml;
-    exports com.mycompany.securityhub;
-}
