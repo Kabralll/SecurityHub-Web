daa6241d2fc3ed16e8448018cb260376eb837121
Gustavo Cabral <161985186+Kabralll@users.noreply.github.com>
2025-06-11 11:13:29 -0300

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+import '../style/Footer.css';
+import githubIcon from '../images/github.svg';
+
+const Footer: React.FC = () => {
+    return (
+        <div className='footer'>
+            <div className='footers'>
+                <div className='topFooter'>
+                    <Link to="/">Security Hub</Link>
+                </div>
+                <div className='midFooter'>
+                    <Link to="/suggestions">Fale Conosco</Link>
+                    <Link to="/terms">Termos de Uso</Link>
+                </div>
+                <div className='bottomFooter'>
+                    <a>2025 - Security Hub. Um novo jeito de gerenciar suas senhas.</a><br></br>     
+                </div>
+                <div className='iconsForm'>
+                    <a href='https://github.com/Kabralll/SecurityHub' target='_blank'><img src={githubIcon} className='githubIcon' /></a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/hooks/newsApi.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+
+export const newsApi = () => {
+
+  const [news, setNews] = useState([]);
+  const API_KEY = '7c56229fcfa74e3cab31f6e795b2e00e';
+
+  const getNews = async () => {
+    try {
+      const res = await fetch(`https://newsapi.org/v2/everything?q=CyberSecurity AND Password&language=en&pageSize=12&sortBy=publishedAt&apiKey=${API_KEY}`);//só consigo usar variavel no texto se ele tivir entre aceento grave (`)
+      const json = await res.json();
+      setNews(json.articles);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+    return news; //retorna todas as noticias coletadas
+  };
